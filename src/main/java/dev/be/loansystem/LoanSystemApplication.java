065362e693513bc8d6d91e465b9099c1d882310e
Kimhyeyun <cqqwer@gmail.com>
2023-03-13 21:10:51 +0900

    #1 - 프로젝트 생성 및 초기 디펜던시 추가

---
+++ src/main/java/dev/be/loansystem/LoanSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.be.loansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanSystemApplication.class, args);
+    }
+
+}
