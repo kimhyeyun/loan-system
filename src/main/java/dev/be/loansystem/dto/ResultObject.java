9ad1fd2603a02a8b12beb45b44f05f0369fef43a
Kimhyeyun <cqqwer@gmail.com>
2023-03-14 23:52:30 +0900

    #6 - 결과와 응답을 공통적인 포맷으로 가져오고 받기 위한 공통 클래스 생성

---
+++ src/main/java/dev/be/loansystem/dto/ResultObject.java
@@ -0,0 +1,37 @@
+package dev.be.loansystem.dto;
+
+import dev.be.loansystem.exception.BaseException;
+import dev.be.loansystem.exception.ResultType;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultObject implements Serializable {
+
+    public String code;
+    public String desc;
+
+    public ResultObject(ResultType resultType) {
+        this.code = resultType.getCode();
+        this.desc = resultType.getDesc();
+    }
+
+    public ResultObject(ResultType resultCode, String message) {
+        this.code = resultCode.getCode();
+        this.desc = message;
+    }
+
+    public ResultObject(BaseException e) {
+        this.code = e.getCode();
+        this.desc = e.getDesc();
+    }
+
+    public static ResultObject getSuccess() {
+        return new ResultObject(ResultType.SUCCESS);
+    }
+}
