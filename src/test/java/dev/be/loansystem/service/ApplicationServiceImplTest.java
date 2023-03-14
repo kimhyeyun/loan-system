354021fc7f895052efd3c67d4a6e9509ae966433
Kimhyeyun <cqqwer@gmail.com>
2023-03-15 01:49:56 +0900

    #18 - 대출 신청 조회 서비스 로직 테스트 작성

--- src/test/java/dev/be/loansystem/service/ApplicationServiceImplTest.java
+++ src/test/java/dev/be/loansystem/service/ApplicationServiceImplTest.java
@@ -2,6 +2,7 @@ package dev.be.loansystem.service;
 
 import dev.be.loansystem.domain.Application;
 import dev.be.loansystem.dto.ApplicationDTO.*;
+import dev.be.loansystem.exception.BaseException;
 import dev.be.loansystem.repository.ApplicationRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,8 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -49,5 +52,29 @@ class ApplicationServiceImplTest {
         Response actual = applicationService.create(request);
 
         assertThat(actual.getName()).isSameAs(entity.getName());
+    }
+
+    @Test
+    void Should_ReturnResponseOfExistApplicationEntity_When_RequestExistsApplicationId() {
+        Long findId = 1L;
+
+        Application entity = Application.builder()
+                .applicationId(1L)
+                .build();
+
+        when(applicationRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        Response actual = applicationService.get(1L);
+
+        assertThat(actual.getApplicationId()).isSameAs(findId);
+    }
+
+    @Test
+    void Should_ThrowException_When_RequestNotExistApplicationId(){
+        Long findId = 2L;
+
+        when(applicationRepository.findById(findId)).thenThrow(BaseException.class);
+
+        assertThrows(BaseException.class, () -> applicationService.get(findId));
     }
 }
