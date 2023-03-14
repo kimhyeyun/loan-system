70954de3805773d5fbc57e1d84b05daa0e1b5753
Kimhyeyun <cqqwer@gmail.com>
2023-03-15 01:50:07 +0900

    #18 - 대출 신청 조회 컨트롤러 구현

--- src/main/java/dev/be/loansystem/controller/ApplicationController.java
+++ src/main/java/dev/be/loansystem/controller/ApplicationController.java
@@ -4,20 +4,22 @@ import dev.be.loansystem.dto.ApplicationDTO.*;
 import dev.be.loansystem.dto.ResponseDTO;
 import dev.be.loansystem.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/applications")
-public class ApplicationController extends AbstractController{
+public class ApplicationController extends AbstractController {
 
     private final ApplicationService applicationService;
 
     @PostMapping
     public ResponseDTO<Response> create(@RequestBody Request request) {
         return ok(applicationService.create(request));
+    }
+
+    @GetMapping("/{applicationId}")
+    public ResponseDTO<Response> get(@PathVariable Long applicationId) {
+        return ok(applicationService.get(applicationId));
     }
 }
