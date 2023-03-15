8667d1e745f697099353806e66dc1593f2b99a88
Kimhyeyun <cqqwer@gmail.com>
2023-03-15 22:14:01 +0900

    #30 - 약관 전체 조회 기능 비즈니스 로직 구현

--- src/main/java/dev/be/loansystem/service/TermsService.java
+++ src/main/java/dev/be/loansystem/service/TermsService.java
@@ -2,7 +2,11 @@ package dev.be.loansystem.service;
 
 import dev.be.loansystem.dto.TermsDTO.*;
 
+import java.util.List;
+
 public interface TermsService {
 
     Response create(Request request);
+
+    List<Response> getAll();
 }
