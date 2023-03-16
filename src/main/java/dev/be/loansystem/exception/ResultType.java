5717baa78bd0bbfd564b48b3e5d926b0e727d78e
Kimhyeyun <cqqwer@gmail.com>
2023-03-17 00:28:26 +0900

    #36 - 파일 조회 시 없는 경우 표시할 오류 추가

--- src/main/java/dev/be/loansystem/exception/ResultType.java
+++ src/main/java/dev/be/loansystem/exception/ResultType.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 public enum ResultType {
 
     SUCCESS("0000", "success"),
+
+    // file = 4000
+    NOT_EXIST("4001","file not exist"),
     SYSTEM_ERROR("9000", "system error");
 
     private final String code;
