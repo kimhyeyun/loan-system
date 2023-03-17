bcd121e42cfa6567705428bd56c17cad9e243cc8
Kimhyeyun <cqqwer@gmail.com>
2023-03-17 20:59:05 +0900

    #36 - 전체 파일 삭제 기능 비즈니스 로직 구현

--- src/main/java/dev/be/loansystem/service/FileStorageServiceImpl.java
+++ src/main/java/dev/be/loansystem/service/FileStorageServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -57,5 +58,10 @@ public class FileStorageServiceImpl implements FileStorageService{
         } catch (Exception e) {
             throw new BaseException(ResultType.SYSTEM_ERROR);
         }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(Path.of(uploadPath).toFile());
     }
 }
