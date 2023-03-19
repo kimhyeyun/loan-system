65b83982ad2106b41ef337feebce8fb0bca9efc0
Kimhyeyun <cqqwer@gmail.com>
2023-03-19 19:18:16 +0900

    #48 - 대출 잔여금 엔티티 구현

---
+++ src/main/java/dev/be/loansystem/domain/Balance.java
@@ -0,0 +1,32 @@
+package dev.be.loansystem.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Where;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Where(clause = "is_deleted=false")
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    public Long balanceId;
+
+    @Column(columnDefinition = "bigint NOT NULL COMMENT '신청 ID'")
+    private Long applicationId;
+
+    @Column(columnDefinition = "decimal(15,2) NOT NULL COMMENT '잔여 대출 금액'")
+    private BigDecimal balance;
+}
