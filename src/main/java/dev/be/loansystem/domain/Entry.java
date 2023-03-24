32be459edd5987fb15343fa6f85e2183e4fb14ef
Kimhyeyun <cqqwer@gmail.com>
2023-03-24 22:21:41 +0900

    #53 - `BaseEntity` 상속 추가

--- src/main/java/dev/be/loansystem/domain/Entry.java
+++ src/main/java/dev/be/loansystem/domain/Entry.java
@@ -17,7 +17,7 @@ import java.math.BigDecimal;
 @DynamicInsert
 @DynamicUpdate
 @Where(clause = "is_deleted=false")
-public class Entry {
+public class Entry extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
